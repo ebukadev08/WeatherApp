2e5e439cbcd049b435a93a289f5f8809fe1e321b
Ebuka <ebukaopara180@gmail.com>
2025-06-11 13:28:43 +0100

    updated

--- server.js
+++ server.js
@@ -2,24 +2,19 @@ const express = require("express");
 const axios = require("axios");
 const app = express();
 
-// Set the view engine to EJS
+
 app.set("view engine", "ejs");
 
-// Serve the public folder as static files
 app.use(express.static("public"));
 
-// Render the index template with default values for weather and error
 app.get("/", (req, res) => {
   res.render("index", { weather: null, error: null });
 });
 
-// Handle the /weather route
 app.get("/weather", async (req, res) => {
-  // Get the city from the query parameters
   const city = req.query.city;
   const apiKey = "32880b224572c22d51585bc8822a0f12";
 
-  // Add your logic here to fetch weather data from the API
   const APIUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
    let weather;
    let error = null;
@@ -30,11 +25,9 @@ app.get("/weather", async (req, res) => {
      weather = null;
      error = "Error, Please try again";
    }
-  // Render the index template with the weather data and error message
   res.render("index", { weather, error });
 });
 
-// Start the server and listen on port 3000 or the value of the PORT environment variable
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
